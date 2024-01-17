84fbb59b881aece0ca55b0ebb08129c5555c46d5
Artur Yumaev <art.yumaev@gmail.com>
2024-01-18 01:00:00 +0300

    worker pool

---
+++ worker-pool/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+const (
+	tasksNum = 100
+)
+
+func main() {
+	wg := &sync.WaitGroup{}
+	wg.Add(tasksNum)
+
+	tasksCh := make(chan int)
+
+	workers := runtime.NumCPU()
+
+	for w := 0; w < workers; w++ {
+		w := w
+
+		go func() {
+			for task := range tasksCh {
+				fmt.Printf("worker %d has done task %d\n", w, task)
+				wg.Done()
+			}
+		}()
+	}
+
+	for t := 0; t < tasksNum; t++ {
+		tasksCh <- t
+	}
+	close(tasksCh)
+
+	wg.Wait()
+}
